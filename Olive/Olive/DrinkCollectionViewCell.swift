9df2f45ba7bd99291339c3d69adcb03994d4ee82
Justin Loew <jloloew@gmail.com>
2014-11-23 08:10:14 -0600

    undo

--- Olive/Olive/DrinkCollectionViewCell.swift
+++ Olive/Olive/DrinkCollectionViewCell.swift
@@ -11,15 +11,20 @@ import CoreData
 
 class DrinkCollectionViewCell: UICollectionViewCell {
 	
-	var drinkName: UILabel!
-	var drinkMatch: UILabel!
-	var icon: UIImageView!
-	var drink: Drink! {
+	@IBOutlet weak var drinkName: UILabel!
+	@IBOutlet weak var drinkMatch: UILabel!
+	@IBOutlet weak var icon: UIImageView!
+	@IBOutlet var drink: Drink! {
 		didSet {
 			drinkName.text = drink.name
 			drinkMatch.text = "\(matchAccuracyForDrink(drink))% match"
 			icon.image = UIImage(data: drink.image)
 		}
 	}
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
 	
 }
